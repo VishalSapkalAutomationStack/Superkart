e9ee0b27dbfca81cd7e1a156c8674eed2b5bc5ed
VishalSapkalAutomationStack <144924513+VishalSapkalAutomationStack@users.noreply.github.com>
2026-05-03 22:41:30 +0530

    Update deploy_hf.py

--- deploy_hf.py
+++ deploy_hf.py
@@ -5,15 +5,18 @@ api = HfApi()
 
 repo_id = "Vishal1986/Superkart"
 
+token = os.getenv("HF_TOKEN")
+
 api.upload_file(
     path_or_fileobj="final_model.pkl",
     path_in_repo="final_model.pkl",
     repo_id=repo_id,
-    repo_type="space"
+    repo_type="space",
+    token=token
 )
 
-api.upload_file("app.py", "app.py", repo_id, repo_type="space")
-api.upload_file("requirements.txt", "requirements.txt", repo_id, repo_type="space")
-api.upload_file("Dockerfile", "Dockerfile", repo_id, repo_type="space")
+api.upload_file("app.py", "app.py", repo_id, repo_type="space", token=token)
+api.upload_file("requirements.txt", "requirements.txt", repo_id, repo_type="space", token=token)
+api.upload_file("Dockerfile", "Dockerfile", repo_id, repo_type="space", token=token)
 
 print("Deployment completed!")
