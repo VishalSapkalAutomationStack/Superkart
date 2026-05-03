04d4aa47207758c8816e40ba38c9ffa227e89538
VishalSapkalAutomationStack <144924513+VishalSapkalAutomationStack@users.noreply.github.com>
2026-05-03 22:57:10 +0530

    Update deploy_hf.py

--- deploy_hf.py
+++ deploy_hf.py
@@ -2,21 +2,18 @@ from huggingface_hub import HfApi
 import os
 
 api = HfApi()
-
 repo_id = "Vishal1986/Superkart"
+token = os.environ["HF_TOKEN"]
 
-token = os.getenv("HF_TOKEN")
+files = ["final_model.pkl", "app.py", "requirements.txt", "Dockerfile"]
 
-api.upload_file(
-    path_or_fileobj="final_model.pkl",
-    path_in_repo="final_model.pkl",
-    repo_id=repo_id,
-    repo_type="space",
-    token=token
-)
+for file in files:
+    api.upload_file(
+        path_or_fileobj=file,
+        path_in_repo=file,
+        repo_id=repo_id,
+        repo_type="space",
+        token=token
+    )
 
-api.upload_file("app.py", "app.py", repo_id, repo_type="space", token=token)
-api.upload_file("requirements.txt", "requirements.txt", repo_id, repo_type="space", token=token)
-api.upload_file("Dockerfile", "Dockerfile", repo_id, repo_type="space", token=token)
-
-print("Deployment completed!")
+print("Deployment completed successfully!")
